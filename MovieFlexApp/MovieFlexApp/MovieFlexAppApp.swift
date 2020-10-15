1b74c21b2ef68a3217ac82d52db328cc3ac68733
AhmedRagab <aragab22@gmail.com>
2020-10-16 00:53:22 +0200

    project setup

---
+++ MovieFlexApp/MovieFlexApp/MovieFlexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlexAppApp.swift
+//  MovieFlexApp
+//
+//  Created by Ahmed Ragab on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
